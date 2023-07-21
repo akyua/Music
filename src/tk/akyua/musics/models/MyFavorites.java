86baa24506c26f9411a64c25261db0af4feaf3bc
Gabriel <gabrielnovatoo@gmail.com>
2023-07-21 11:26:44 -0300

    Translated the answer to english

--- src/tk/akyua/musics/models/MyFavorites.java
+++ src/tk/akyua/musics/models/MyFavorites.java
@@ -3,9 +3,9 @@ package src.tk.akyua.musics.models;
 public class MyFavorites {
     public void include(Audio audio){
         if(audio.getClassification() >= 9){
-            System.out.println(audio.getTitle() + " é considerado uma das preferidas por todo");
+            System.out.println(audio.getTitle() + "  is considered one of the favorites by everyone.");
         } else {
-            System.out.println(audio.getTitle() + " é uma das que as pessoas estão gostando");
+            System.out.println(audio.getTitle() + " is one of the ones that people are enjoying.");
         }
     }
 }
